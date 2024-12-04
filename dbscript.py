28def3a37ab7b0d7345cb143d38440209c6c59d2
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-12-04 18:39:33 +0530

    pushed

--- dbscript.py
+++ dbscript.py
@@ -3,10 +3,10 @@ import pymongo
 # User credentials will be saved in 'new' database 
 
 # Prerequisite : MongoDB Compass
-client = pymongo.MongoClient("localhost", 27017)
+# client = pymongo.MongoClient("localhost", 27017)
 
 # In case you want to use MongoDB Atlas instead of MongoDB Compass
-# client = pymongo.MongoClient("mongo-db-connection-string")
+client = pymongo.MongoClient("mongodb+srv://ayushdhyani478:HCursQszoq3LFFWc@cluster0.1naga.mongodb.net/")
 
 db = client.new
 
